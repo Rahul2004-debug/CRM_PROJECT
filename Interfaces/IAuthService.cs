6f401ad7c42609677d777b20fdc42d854a38f733
Harshith Gowda <harshith.gowda@mrisoftware.com>
2026-03-06 09:33:23 +0530

    basic structure included attentication

---
+++ Interfaces/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using CRM.API.DTOs;
+
+namespace CRM.API.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<string> Register(RegisterDto dto);
+        Task<string> Login(LoginDto dto);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,14 +1,68 @@
+using CRM.API.Data;
+using CRM.API.Helpers;
+using CRM.API.Interfaces;
+using CRM.API.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container
+// Add Controllers
 builder.Services.AddControllers();
 
+// Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Database
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Services
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<JwtHelper>();
+
+// CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+// JWT Authentication
+var key = Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateIssuerSigningKey = true,
+        ValidateLifetime = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(key)
+    };
+});
+
 var app = builder.Build();
 
-// Configure HTTP pipeline
+// Serve HTML pages
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+// CORS
+app.UseCors("AllowAll");
+
+// Swagger
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,6 +71,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// Authentication
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
